72ebc36aaa21755f850ae94852e06f1797438af2
MehrabChowdhury <93445792+FaysalMehrab@users.noreply.github.com>
2025-06-16 03:36:37 +0600

    Update server.py

--- server.py
+++ server.py
@@ -15,14 +15,24 @@ class HumanDetectionTrack(VideoStreamTrack):
     def __init__(self, track):
         super().__init__()
         self.track = track
+        self.frame_count = 0
+        self.last_annotated = None
 
     async def recv(self):
         frame = await self.track.recv()
         img = frame.to_ndarray(format="bgr24")
-        img = cv2.resize(img, (224, 224))  # Add before YOLO inference
-        results = model(img, classes=[0])  # Class 0 is 'person'
-        annotated_img = results[0].plot()
-        annotated_img = cv2.resize(annotated_img, (frame.width, frame.height))  # Resize back for display
+        self.frame_count += 1
+
+        # Only run YOLO every 5th frame, reuse last result for others
+        if self.frame_count % 5 == 0 or self.last_annotated is None:
+            img_resized = cv2.resize(img, (160, 160))  # Lower resolution for faster inference
+            results = model(img_resized, classes=[0])
+            annotated_img = results[0].plot()
+            annotated_img = cv2.resize(annotated_img, (frame.width, frame.height))
+            self.last_annotated = annotated_img
+        else:
+            annotated_img = self.last_annotated
+
         annotated_frame = VideoFrame.from_ndarray(annotated_img, format="bgr24")
         annotated_frame.pts = frame.pts
         annotated_frame.time_base = frame.time_base

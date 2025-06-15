b1d1ef794dce8acd535b7b3f2642849d9e78536d
MehrabChowdhury <93445792+FaysalMehrab@users.noreply.github.com>
2025-06-16 03:36:51 +0600

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
 const pc = new RTCPeerConnection();
 const remoteVideo = document.getElementById("remoteVideo");
 
-navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+navigator.mediaDevices.getUserMedia({ video: { width: 320, height: 240 }, audio: false })
     .then(stream => {
         stream.getTracks().forEach(track => pc.addTrack(track, stream));
     })
